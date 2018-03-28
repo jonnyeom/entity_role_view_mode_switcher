e29f3ff86b8dd5e2d97625ef2c7299143393c320
Chris Snyder <chris@chrissnyder.org>
2018-03-28 09:39:51 -0400

    Initial commit.

---
+++ src/RuleHtmlRouteProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\entity_role_view_mode_switcher;
+
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\Routing\AdminHtmlRouteProvider;
+use Symfony\Component\Routing\Route;
+
+/**
+ * Provides routes for View Mode Switcher Rule entities.
+ *
+ * @see \Drupal\Core\Entity\Routing\AdminHtmlRouteProvider
+ * @see \Drupal\Core\Entity\Routing\DefaultHtmlRouteProvider
+ */
+class RuleHtmlRouteProvider extends AdminHtmlRouteProvider {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRoutes(EntityTypeInterface $entity_type) {
+    $collection = parent::getRoutes($entity_type);
+
+    // Provide your custom entity routes here.
+
+    return $collection;
+  }
+
+}
